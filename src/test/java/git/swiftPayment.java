2a291e1a7f6107c4258a9d7b76c4090c098c7242
BegimaiAibekova <begimai.aibekova@gmail.com>
2023-06-22 12:31:43 +0600

    createdMethodTwo

--- src/test/java/git/swiftPayment.java
+++ src/test/java/git/swiftPayment.java
@@ -4,4 +4,7 @@ public class swiftPayment {
     public void sendMoney(String bankName, String accountNumber){
         System.out.println("Money has been sent");
     }
+    public void sendMoneyBack(String bankName, String accountNumber){
+        System.out.println("Money has been sent back");
+    }
 }
